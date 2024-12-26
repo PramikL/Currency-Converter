163eee07aca677ddd035329f128b3425b45e5eb9
Pramik l Tamang <pramiktamang@gmail.com>
2024-12-26 19:42:00 +0545

    Random

---
+++ practice.js
@@ -0,0 +1,13 @@
+const URL = "https://meowfacts.herokuapp.com/";
+const factspara = document.querySelector("#facts");
+const button = document.querySelector("#btn");
+
+const getdata = async() =>{
+    console.log("Getting data....");
+    let response  = await fetch(URL);
+    console.log(response);
+    let data =await response.json()
+    factspara.innerText = data.data[0];
+}
+
+button.addEventListener("click",getdata);
